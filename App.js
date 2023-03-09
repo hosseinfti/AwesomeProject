631e2855c221d6420fa531852cb67867565e8d8f
Hossein <79690564+hosseinfti@users.noreply.github.com>
2023-03-10 01:48:10 +0330

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//start develop with expo
+
 export default function App() {
   return (
     <View style={styles.container}>
